0abef9226f8d7b20e890aea73e4901e9153c2b7e
mduleone <matt@duleone.com>
2014-10-27 12:22:18 -0400

    Pushing solution

--- ex3/index.js
+++ ex3/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 var express = require("express");
-var path = require("path");
+var bodyparser = require("body-parser");
 var app = express();
+
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.post("/form", function(req, res) {
+    res.end(req.body.str.split("").reverse().join(""));
+});
+
+app.listen(process.argv[2]);

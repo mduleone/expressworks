25ee925f50585415dc604319879159201d7f3832
mduleone <matt@duleone.com>
2014-10-28 15:31:00 -0400

    removed extra line in ex6 solution

--- ex6/index.js
+++ ex6/index.js
@@ -6,7 +6,6 @@ var crypto = require("crypto");
 var app = express();
 var port = process.argv[2];
 
-
 app.put("/message/:id", function(req, res) {
     var id = req.params.id;
     var date = crypto.createHash("sha1").update(new Date().toDateString() + id).digest("hex");

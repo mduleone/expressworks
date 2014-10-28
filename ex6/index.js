1a16a214d1e70bf6940ff955daee90bfcf6b816e
mduleone <matt@duleone.com>
2014-10-28 15:03:12 -0400

    First attempt at solution to ex6

---
+++ ex6/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require("express");
+var crypto = require("crypto");
+
+var app = express();
+var port = process.argv[2];
+
+
+app.put("/message/:id", function(req, res) {
+    var id = req.params.id;
+    var date = crypto.createHash('sha1').update(new Date().toDateString() + id).digest('hex');
+    res.end(date);
+});
+
+app.listen(port);

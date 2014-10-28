7f4a227403332d0c990200a19f4bc80a324c2fbc
mduleone <matt@duleone.com>
2014-10-28 11:36:31 -0400

    updating solution to ex4

--- ex4/index.js
+++ ex4/index.js
@@ -6,6 +6,7 @@ var path = require("path");
 var port = process.argv[2];
 var app = express();
 
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+// app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+app.use(express.static(process.argv[3]));
 
 app.listen(port);

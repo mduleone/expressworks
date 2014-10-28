9d8d259805b8a798994dafd90c250e15c0236238
mduleone <matt@duleone.com>
2014-10-28 15:24:03 -0400

    changing to double quotes in ex6 solution

--- ex6/index.js
+++ ex6/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var express = require("express");
 var crypto = require("crypto");
@@ -9,7 +9,7 @@ var port = process.argv[2];
 
 app.put("/message/:id", function(req, res) {
     var id = req.params.id;
-    var date = crypto.createHash('sha1').update(new Date().toDateString() + id).digest('hex');
+    var date = crypto.createHash("sha1").update(new Date().toDateString() + id).digest("hex");
     res.end(date);
 });
 
